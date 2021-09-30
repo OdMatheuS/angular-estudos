d9ac8fdc6e0ba3cc7c02f6d735c1340e68c3ade1
Matheus Nagot <matheusnagot@hotmail.com>
2021-09-30 18:03:41 -0300

    feat: base p/ servico de auth e post de auth login user

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+ autenticar(user:string,password:string):Observable<any>{
+    return this.http.post('http://localhost:3000/user/login',{
+      userName:user,
+      password:password
+    })
+ }
+
+
+
+}
