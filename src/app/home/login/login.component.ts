b4b9b8ffb3c67506f978ffded762e10124c48a12
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-01 17:24:03 -0300

    feat: consumindo o serviço de autenticação e expondo erro request console

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,26 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public nome: string = '';
+  public senha: string = '';
 
-  public nome:string = ''
-  public senha:string = ''
+  constructor(private authService: AuthService) {}
 
-  constructor(private authService:AuthService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-
+  efetuarLogin(): void {
+    let foo = [this.nome, this.senha];
+    console.log('dados form', foo);
+    this.authService.autenticar(this.nome, this.senha).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err: HttpErrorResponse) => {
+        console.error("Fail Men",err.message);
+      }
+    );
   }
-
-  efetuarLogin():void{
-    let foo = [ this.nome, this.senha]
-    console.log("dados form",foo)
-   this.authService.autenticar(this.nome,this.senha) 
-  }
-
 }
