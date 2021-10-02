0626a65cf8a6a0504cfe03e5d1e3529d95a78c77
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-02 12:45:57 -0300

    feat: validacoes customizadas no formGroup
    utilizando uma fn exportada para validar se naquele input contem apenas letras minusculas

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { NovoUsuario } from './novo-usuario';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { lowerCaseValidator } from './lowerCase.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,27 +18,26 @@ export class NovoUsuarioComponent implements OnInit {
   ngOnInit(): void {
     this.novoUsuarioForm = this.fb.group({
       password: [''],
-      fullName: ['', Validators.required,Validators.minLength(5)],
-      userName: [''],
-      email: ['',Validators.required,Validators.email],
+      fullName: ['', Validators.required, Validators.minLength(5)],
+      userName: ['', [lowerCaseValidator]],
+      email: ['', Validators.required, Validators.email],
     });
   }
 
   postNovoUsuario(): void {
     const novoUsuario = this.novoUsuarioForm.getRawValue();
     if (this.novoUsuarioForm.valid) {
-      
-    this.cadastro.cadastrarNovoUsuario(novoUsuario).subscribe(
-      (value: NovoUsuario) => {
-        (res: HttpResponse<NovoUsuario>) => {
-          console.log(res);
-          console.log(value);
-        };
-      },
-      (err: HttpErrorResponse) => {
-        console.log(err);
-      }
-    );
+      this.cadastro.cadastrarNovoUsuario(novoUsuario).subscribe(
+        (value: NovoUsuario) => {
+          (res: HttpResponse<NovoUsuario>) => {
+            console.log(res);
+            console.log(value);
+          };
+        },
+        (err: HttpErrorResponse) => {
+          console.log(err);
+        }
+      );
     }
   }
 }
