65d9c285ab622bee1dbd080c48130938a8db3562
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-02 10:27:54 -0300

    feat: serviço de novo usuário
    post cadastro usuario + interface novo-usuario

--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NovoUsuario } from './novo-usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NovoUsuarioService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  cadastrarNovoUsuario(params:NovoUsuario):Observable<NovoUsuario>{
+    return this.http.post<NovoUsuario>('http://localhost:3000/user/singup',params)
+  }
+
 }
