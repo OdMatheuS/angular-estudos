0b23cacc3b2a874965ae1294fc99e16ca7e258a8
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-03 10:57:55 -0300

    feat: pegando infos do meu observable e passando pro meu template via pipe async
    implementacao component cabecalho exportando seu modulo+fazendo um subscribe no observable user$ e
    consumindo no template via "pipe async as data"

--- src/app/components/cabecalho/cabecalho.component.ts
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/auth/usuario/usuario.service';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
-  styleUrls: ['./cabecalho.component.css']
+  styleUrls: ['./cabecalho.component.css'],
 })
-export class CabecalhoComponent implements OnInit {
+export class CabecalhoComponent {
+  public user$ = this.usuarioService.retornaUsuario();
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  ngOnInit(): void {
+  logout(): void {
+    this.usuarioService.logout();
+    this.router.navigate(['']);
   }
-
 }
