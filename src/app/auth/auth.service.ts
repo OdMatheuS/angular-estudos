eb27546990eccf9d4aa62e2d228926b707e7f9d1
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-03 10:32:58 -0300

    feat: implementando servico autenticacao com token jwt localStorage
    implementado metodo autenticacao, pega jwt no header e salva no localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient, private usuarioService: UsuarioService) {}
 
-  constructor(private http:HttpClient) { }
-
- autenticar(user:string,password:string):Observable<any>{
-    return this.http.post('http://localhost:3000/user/login',{
-      userName:user,
-      password:password
-    })
- }
-
-
-
+  autenticar(user: string, password: string): Observable<HttpResponse<any>> {
+    return this.http
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: user,
+          password: password,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
+  }
 }
