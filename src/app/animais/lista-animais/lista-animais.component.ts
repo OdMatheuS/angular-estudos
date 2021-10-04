b829090aac08a806b2b3a65eff417f402ecdc6c6
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-04 10:08:17 -0300

    feat: implementacao servico e retorno imagens animais

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/auth/usuario/usuario.service';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
+  public animais!: Animais;
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
   }
-
 }
