285850c750876317d7fef6a9dec8af3758b1e7a1
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-05 09:52:50 -0300

    feat: criacao do servico e interface comentarios

--- src/app/animais/detalhe-animal/comentarios/comentarios.service.ts
+++ src/app/animais/detalhe-animal/comentarios/comentarios.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Comentario, Comentarios } from './comentarios';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComentariosService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  buscarComentarios(comentarioId: number): Observable<Comentarios> {
+    return this.http.get<Comentarios>(`${environment.apiUrl}/photos/${comentarioId}/comments`);
+  }
+
+  incluirComentario(comentarioId: number, textComment: string): Observable<Comentario> {
+    return this.http.post<Comentario>(`${environment.apiUrl}/photos/${comentarioId}/comments`, {
+      textComment,
+    });
+  }
 }
